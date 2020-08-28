7ba1c950f8bd7af736c7308ccf0b6f46cb9a6fcc
kimizhengchong <zhengchong96@163.com>
2020-08-28 15:49:07 +0800

    删除库

--- baidu-shop-parent/baidu-shop-service/baidu-shop-service-xxx/src/main/java/com/baidu/shop/mapper/CategoryMapper.java
+++
@@ -1,7 +0,0 @@
-package com.baidu.shop.mapper;
-
-import com.baidu.shop.entity.CategoryEntity;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface CategoryMapper extends Mapper<CategoryEntity> {
-}
