66773144a67befbd5b58b301fb4bb4639809ee86
kimizhengchong <zhengchong96@163.com>
2020-08-28 21:15:55 +0800

    增删改查

--- baidu-shop-parent/baidu-shop-service/baidu-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/CategoryServiceImpl.java
+++ baidu-shop-parent/baidu-shop-service/baidu-shop-service-xxx/src/main/java/com/baidu/shop/service/impl/CategoryServiceImpl.java
@@ -1,13 +1,17 @@
 package com.baidu.shop.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baidu.shop.base.Result;
 import com.baidu.shop.entity.CategoryEntity;
 import com.baidu.shop.mapper.CategoryMapper;
 import com.baidu.shop.service.BaseApiService;
 import com.baidu.shop.service.CategoryService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.google.gson.JsonObject;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.mapper.entity.Example;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -19,18 +23,71 @@ import java.util.List;
 @RestController
 public class CategoryServiceImpl extends BaseApiService implements CategoryService {
 
-    @Autowired
+    @Resource
     private CategoryMapper categoryMapper;
+
 
     @Override
     public Result<List<CategoryEntity>> getCategoryByPid(Integer pid) {
-
         CategoryEntity categoryEntity = new CategoryEntity();
-
         categoryEntity.setParentId(pid);
-
         List<CategoryEntity> list = categoryMapper.select(categoryEntity);
-
         return this.setResultSuccess(list);
     }
+
+    @Transactional
+    @Override
+    public Result<JSONObject> addCategory(CategoryEntity entity) {
+
+        CategoryEntity parentCateEntity = new CategoryEntity();
+        parentCateEntity.setId(entity.getParentId());
+        parentCateEntity.setIsParent(1);
+        categoryMapper.updateByPrimaryKeySelective(parentCateEntity);
+
+            categoryMapper.insertSelective(entity);
+        return this.setResultSuccess();
+    }
+
+
+    @Transactional
+    @Override
+    public Result<JSONObject> editCategory(CategoryEntity entity) {
+
+        categoryMapper.updateByPrimaryKeySelective(entity);
+
+        return this.setResultSuccess();
+    }
+
+    @Transactional
+    @Override
+    public Result<JsonObject> delCategory(Integer id) {
+
+        //验证传入的id是否有效,并且查询出来的数据对接下来的程序有用
+        CategoryEntity categoryEntity = categoryMapper.selectByPrimaryKey(id);
+        if (categoryEntity == null) {
+            return this.setResultError("当前id不存在");
+        }
+        //判断当前节点是否为父节点
+        if(categoryEntity.getIsParent() == 1){
+            return this.setResultError("当前节点为父节点,不能删除");
+        }
+
+        //构建条件查询 通过当前被删除节点的parentid查询数据
+        Example example = new Example(CategoryEntity.class);
+        example.createCriteria().andEqualTo("parentId",categoryEntity.getParentId());
+        List<CategoryEntity> list = categoryMapper.selectByExample(example);
+        //如果查询出来的数据只有一条
+        if(list.size() == 1){//将父节点的isParent状态改为0
+
+            CategoryEntity parentCateEntity = new CategoryEntity();
+            parentCateEntity.setId(categoryEntity.getParentId());
+            parentCateEntity.setIsParent(0);
+            categoryMapper.updateByPrimaryKeySelective(parentCateEntity);
+        }
+
+        categoryMapper.deleteByPrimaryKey(id);//执行删除
+
+        return this.setResultSuccess();
+    }
+
 }
